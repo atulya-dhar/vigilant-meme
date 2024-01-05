b20edaf28cc94b9405e01c9e0d33cbc3cc80f247
Atulya Dhar <atulyadhar0428@gmail.com>
2024-01-05 18:07:08 +0530

    Understanding DOM & getElementById Method

---
+++ Task - 3/index.js
@@ -0,0 +1,14 @@
+const headingChanger = document.getElementById('main-heading');
+headingChanger.textContent = 'Fruit World';
+
+const header = document.getElementById('header');
+header.style.color = 'orange';
+header.style.backgroundColor = 'green';
+header.style.borderBottom = '10px solid orange';
+
+const basketChanger = document.getElementById('basket-heading');
+basketChanger.style.color = 'green';
+
+const thanksChanger = document.getElementById('thanks');
+thanksChanger.innerHTML = '<p> Please visit us again </p>';
+thanksChanger.style.fontWeight = 'bold';
