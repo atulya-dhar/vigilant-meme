2b34bb9239f1508c3ee19f6c8657bfee73049625
Atulya Dhar <atulyadhar0428@gmail.com>
2024-01-05 19:10:58 +0530

    getElementsByClassName Method

---
+++ Task - 4/index.js
@@ -0,0 +1,6 @@
+const fruitManipulator = document.getElementsByClassName('fruit');
+fruitManipulator[2].style.backgroundColor = 'yellow';
+
+for(let i=0; i<=fruitManipulator.length; i++){
+    fruitManipulator[i].style.fontWeight = 'bold';
+}
