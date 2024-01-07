e50da4a1f9f272aef26750dfac8dd0e47bc429d3
Atulya Dhar <atulyadhar0428@gmail.com>
2024-01-07 14:26:10 +0530

    Update index.js

--- Task - 4/index.js
+++ Task - 4/index.js
@@ -1,6 +1,6 @@
 const fruitManipulator = document.getElementsByClassName('fruit');
 fruitManipulator[2].style.backgroundColor = 'yellow';
 
-for(let i=0; i<=fruitManipulator.length; i++){
+for(let i=0; i<=fruitManipulator.length-1; i++){
     fruitManipulator[i].style.fontWeight = 'bold';
 }
